fd5d21d42136218acb9c5aafc2ffc432db6ac184
DruzhyninB <druzhyninbogdan@gmail.com>
2016-01-03 20:17:05 +0200

    add browser version logick

---
+++ javascripts/oldversion.js
@@ -0,0 +1,6 @@
+﻿$(window).ready(function () {
+    if ($('html').hasClass('no-video')) {
+        alert('For correct display of the content, please update your browser to a newer version');
+    }
+
+});
